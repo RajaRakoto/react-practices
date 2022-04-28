982ffca24e62afbb14f12edc6f78b92356085a1f
raja <raja.rakoto7@gmail.com>
2022-04-28 16:32:04 +0300

    src: init all

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom/client';
+/* styles */
+import './index.min.css';
+/* components */
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<Fragment>
+		<p>
+			We believe we know that if you integrate intuitively then you may also
+			disintermediate perfectly. Without macro-vertical CAE, you will lack
+			architectures. A company that can streamline elegantly will (at some
+			undefined point of time in the future) be able to engineer easily. Is it
+			more important for something to be leading-edge or to be best-of-breed?
+			The portals factor can be delivered as-a-service to wherever it’s intended
+			to go – mobile. Our infinitely reconfigurable feature set is unparalleled,
+			but our sexy raw bandwidth and easy use is frequently considered a
+			terrific achievement. Without development, you will lack architectures.
+			Without preplanned cyber-Total Quality Control, aggregation are forced to
+			become cross-media? We think that most co-branded splash pages use far too
+			much Rails, and not enough HTTP. Without niches, you will lack
+			affiliate-based compliance. Quick: do you have a infinitely reconfigurable
+			scheme for coping with emerging methodologies? Is it more important for
+			something to be leading-edge or to be best-of-breed? The portals factor
+			can be summed up in one word: open-source. A company that can synthesize
+			courageously will (eventually) be able to orchestrate correctly. Our
+			functionality is unparalleled, but our robust iteration and user-proof
+			configuration is always considered a terrific achievement. Our infinitely
+			reconfigurable feature set is unparalleled in the industry, but our
+			granular integrated, value-added convergence and easy configuration is
+			always considered a terrific achievement.
+		</p>
+	</Fragment>,
+);
+reportWebVitals();
