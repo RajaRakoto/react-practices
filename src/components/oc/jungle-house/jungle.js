c02952d4deea5df1402dad34982d81dfa6feee1e
raja <raja.rakoto7@gmail.com>
2022-04-29 17:27:25 +0300

    add utils docs & valid getObjectElementType()

--- src/components/oc/jungle-house/jungle.js
+++ src/components/oc/jungle-house/jungle.js
@@ -4,14 +4,14 @@ import { plantList } from '../../../data/plantList';
 
 /// ----------- UTILITIES -----------
 /**
- * @description extraire un element particulier dans un tableau d'objet
+ * @description extraire un element particulier dans un tableau d'objet (ex: le prix d'un olivier)
  * @param {object} obj - tableau d'objet a filtrer
  * @param {string} id - identifiant de l'element de l'objet
  * @param {string} key - le mot cle a extraire (sa valeur)
  * @returns {*} la valeur de l'argument `key` de l'element portant l'identifiant `id` de l'objet `obj`
- * @example getObjectElement(plantList, '3sd', 'price');
+ * @example getObjectElementValue(plantList, '5pl', 'price');
  */
-const getObjectElement = (obj, id, key) => {
+const getObjectElementValue = (obj, id, key) => {
 	const result = obj.filter(obj => obj.id === id);
 
 	if (key === 'name') {
@@ -23,10 +23,43 @@ const getObjectElement = (obj, id, key) => {
 	}
 };
 
+/**
+ * @description extraire une liste de valeur unique dans un tableau d'objet (ex: les differentes type de categorie de plante)
+ * @param {object} obj - tableau d'objet a filter
+ * @param {string} key - le mot cle a extraire (liste de sa valeur unique)
+ * @returns {array} la liste de la valeur unique contenant dans le tableau d'objet
+ * @example getObjectElementType(plantList, 'category')
+ */
+const getObjectElementType = (obj, key) => {
+	if (key === 'category') {
+		var result = obj.reduce(
+			(acc, obj) =>
+				acc.includes(obj.category) ? acc : acc.concat(obj.category),
+			[],
+		);
+	} else if (key === 'name') {
+		result = obj.reduce(
+			(acc, obj) => (acc.includes(obj.name) ? acc : acc.concat(obj.name)),
+			[],
+		);
+	} else if (key === 'id') {
+		result = obj.reduce(
+			(acc, obj) => (acc.includes(obj.id) ? acc : acc.concat(obj.id)),
+			[],
+		);
+	} else if (key === 'price') {
+		result = obj.reduce(
+			(acc, obj) => (acc.includes(obj.price) ? acc : acc.concat(obj.price)),
+			[],
+		);
+	}
+	return result;
+};
+
 /// ----------- COMPONENTS -----------
 function Header() {
 	const description = (
-		<p>Ici achetez toutes les plantes dont vous avez toujours rêvé 🌱</p>
+		<p>Ici achetez toutes les plantes dont vous avez toujours rêvé</p>
 	);
 	const title = 'Jungle house';
 	return (
@@ -45,57 +78,46 @@ function Cart() {
 		<div id="cart">
 			<h4>🛒 Panier</h4>
 			<ul>
-				<li>Monstera: {getObjectElement(plantList, '1ed', 'price') + unity}</li>
-				<li>Yucca: {getObjectElement(plantList, '4kk', 'price') + unity}</li>
-				<li>Olivier: {getObjectElement(plantList, '5pl', 'price') + unity}</li>
+				<li>
+					Monstera: {getObjectElementValue(plantList, '1ed', 'price') + unity}
+				</li>
+				<li>
+					Yucca: {getObjectElementValue(plantList, '4kk', 'price') + unity}
+				</li>
+				<li>
+					Olivier: {getObjectElementValue(plantList, '5pl', 'price') + unity}
+				</li>
 			</ul>
 			<h5>
 				<span>TOTAL:</span>{' '}
-				{getObjectElement(plantList, '1ed', 'price') +
-					getObjectElement(plantList, '4kk', 'price') +
-					getObjectElement(plantList, '5pl', 'price')}
+				{getObjectElementValue(plantList, '1ed', 'price') +
+					getObjectElementValue(plantList, '4kk', 'price') +
+					getObjectElementValue(plantList, '5pl', 'price')}
 			</h5>
 		</div>
 	);
 }
 
-// NOTE: reduce() - extrait une liste de valeur unique par un mot cle (category)
-// const getUniqueList = () => {
-// 	const categories = plantList.reduce(
-// 		(acc, plant) =>
-// 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
-// 		[],
-// 	);
-// };
-
 // TODO: work
 function Shopping() {
-	// const getCategoryList = () => {
-	// 	const categories = plantList.reduce(
-	// 		(acc, plant) =>
-	// 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
-	// 		[],
-	// 	);
-
-	// 	return categories;
-	// };
-
-	// console.table(categories);
+	const categoriesList = getObjectElementType(plantList, 'category');
 
 	return (
-		// <div>
-		// 	<ul>
-		// 		{categories.map(cat => (
-		// 			<li key={cat}>{cat}</li>
-		// 		))}
-		// 	</ul>
-		// 	<ul>
-		// 		{plantList.map(plant => (
-		// 			<li key={plant.id}>{plant.name}</li>
-		// 		))}
-		// 	</ul>
-		// </div>
-		<div></div>
+		<div>
+			{/* liste des differentes types de categorie de plante  */}
+			🌱
+			<ul>
+				{categoriesList.map(category => (
+					<li key={category}>{category}</li>
+				))}
+			</ul>
+			{/* liste de toutes les plantes  */}
+			<ul>
+				{plantList.map(plant => (
+					<li key={plant.id}>{plant.name}</li>
+				))}
+			</ul>
+		</div>
 	);
 }
 
