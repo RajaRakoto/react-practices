166318a4d6e740e7697984b568f6585dc7f1d135
raja <raja.rakoto7@gmail.com>
2022-05-02 22:24:58 +0300

    add specialoffer for plant

--- src/data/plantList.js
+++ src/data/plantList.js
@@ -5,6 +5,7 @@ export const plantList = [
 		category: 'classique',
 		price: 3000,
 		isBestSale: true,
+		isSpecialOffer: false,
 		description:
 			'All the speed he took, all the turns he’d taken and the dripping chassis of a skyscraper canyon.',
 	},
@@ -14,6 +15,7 @@ export const plantList = [
 		category: 'classique',
 		price: 8000,
 		isBestSale: true,
+		isSpecialOffer: false,
 		description:
 			'Das Rahmenkonzept reduziert den Ermessensspielraum von Banken, die für den überwiegenden Teil ihrer Geschäftsaktivitäten über eine IMMZulassung verfügen, aber für sich allein nicht aus.',
 	},
@@ -23,6 +25,7 @@ export const plantList = [
 		category: 'classique',
 		price: 1500,
 		isBestSale: false,
+		isSpecialOffer: false,
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 	},
@@ -31,7 +34,8 @@ export const plantList = [
 		name: 'yucca',
 		category: 'classique',
 		price: 5000,
-		isBestSale: false,
+		isBestSale: true,
+		isSpecialOffer: true,
 		description:
 			'If you transition globally, you may also reintermediate magnetically.',
 	},
@@ -41,6 +45,7 @@ export const plantList = [
 		category: 'extérieur',
 		price: 2200,
 		isBestSale: false,
+		isSpecialOffer: false,
 		description:
 			'Ich bin von je der Ordnung Freund gewesen. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen.',
 	},
@@ -51,6 +56,7 @@ export const plantList = [
 		category: 'extérieur',
 		price: 4600,
 		isBestSale: true,
+		isSpecialOffer: true,
 		description:
 			'To rule over the face of the deep, while a wind from God swept over the cattle, and to the sight and good for food, and that it was good.',
 	},
@@ -60,6 +66,7 @@ export const plantList = [
 		category: 'extérieur',
 		price: 7000,
 		isBestSale: false,
+		isSpecialOffer: false,
 		description:
 			'In every Case, after the Choice of the Members present. The United States shall guarantee to every State in this Constitution shall be so construed as to Prejudice any Claims of the first Article. .',
 	},
@@ -69,6 +76,7 @@ export const plantList = [
 		category: 'plante grasse',
 		price: 3200,
 		isBestSale: false,
+		isSpecialOffer: false,
 		description:
 			'Innertagesliquiditätsrisiken wird noch weiter an Überwachungsinstrumenten gearbeitet. Der gleiche Ansatz ist anzuwenden, wenn das IMM-Modell kein Profil für die CVARisikokapitalanforderung gegen die CVA aufgerechnet werden.',
 	},
@@ -78,6 +86,7 @@ export const plantList = [
 		category: 'plante grasse',
 		price: 3500,
 		isBestSale: false,
+		isSpecialOffer: true,
 		description:
 			'Ex nam agam veri, dicunt efficiantur ad qui, ad legere adversarium sit.',
 	},
