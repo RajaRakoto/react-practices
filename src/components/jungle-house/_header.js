129f651c12ac21ff155955ba379872b0d3d7533c
raja <raja.rakoto7@gmail.com>
2022-05-04 11:26:54 +0300

    refactoring

--- src/components/jungle-house/_header.js
+++ src/components/jungle-house/_header.js
@@ -1,53 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
-export default function Header() {
-	const description = (
-		<p>Ici achetez toutes les plantes dont vous avez toujours rêvé</p>
-	);
-	const title = 'Jungle house';
+/**
+ * @feat - props | Date
+ */
+function Recommendation() {
+	const currentDate = new Date().getMonth();
+	const isSpring = currentDate >= 2 && currentDate <= 5;
 
-	/**
-	 * @feat - condition | props | Date
-	 */
-	const Recommendation = () => {
-		const currentDate = new Date().getMonth();
-		const isSpring = currentDate >= 2 && currentDate <= 5;
-		const RecomNotif = props => {
-			const { status, message } = props;
-			return (
-				<div className={status} role="alert">
-					<strong>Recommendation:</strong> {message}
-				</div>
-			);
-		};
-
-		if (!isSpring) {
-			return (
-				<RecomNotif
-					message="ce n'est pas le moment de rempoter !"
-					status="alert alert-warning"
-				/>
-			);
-		} else {
-			return (
-				<RecomNotif
-					message="c'est le printemps, rempotez !"
-					status="alert alert-success"
-				/>
-			);
-		}
+	const RecomNotif = props => {
+		const { status, message } = props;
+		return (
+			<div className={status} role="alert">
+				<strong>Recommendation:</strong> {message}
+			</div>
+		);
 	};
 
-	return (
-		<Fragment>
-			<div id="banner">
-				<div id="official-logo"></div>
-				<h1 id="title"> {title.toUpperCase()}</h1>
-				<h3 id="description"> {description}</h3>
-			</div>
-			<div id="notification">
-				<Recommendation />
-			</div>
-		</Fragment>
-	);
+	if (!isSpring) {
+		return (
+			<RecomNotif
+				message="ce n'est pas le moment de rempoter !"
+				status="alert alert-warning"
+			/>
+		);
+	} else {
+		return (
+			<RecomNotif
+				message="c'est le printemps, rempotez !"
+				status="alert alert-success"
+			/>
+		);
+	}
+}
+
+export default class Header extends Component {
+	render() {
+		const description = (
+			<p>Ici achetez toutes les plantes dont vous avez toujours rêvé</p>
+		);
+		const title = 'Jungle house';
+		return (
+			<Fragment>
+				<div id="banner">
+					<div id="official-logo"></div>
+					<h1 id="title"> {title.toUpperCase()}</h1>
+					<h3 id="description"> {description}</h3>
+				</div>
+				<div id="notification">
+					<Recommendation />
+				</div>
+			</Fragment>
+		);
+	}
 }
--- src/components/jungle-house/_shopping.js
+++ src/components/jungle-house/_shopping.js
@@ -1,5 +1,5 @@
 /* datas */
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { useMediaQuery } from 'react-responsive';
 /* datas */
 import { plantList } from '../../data/plantList';
@@ -12,143 +12,137 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 /**
- * @feat - map() | ternary conditional | getObjectElementType()
+ * @feat - map() | mui(Button) | react-responsive(mediaquery) | getObjectElementType()
  */
-export default function Shopping() {
+function Categories() {
 	const categoriesList = getObjectElementType(plantList, 'category');
+	const isSmallScreenForCategories = useMediaQuery({
+		query: '(max-width: 537px)',
+	});
 
-	/**
-	 * @feat - map() | mui(Button) | react-responsive(mediaquery)
-	 */
-	const Categories = () => {
-		const isSmallScreenForCategories = useMediaQuery({
-			query: '(max-width: 537px)',
-		});
+	return (
+		<Fragment>
+			<div className="categories-list">
+				<Stack
+					spacing={3}
+					direction={isSmallScreenForCategories ? 'column' : 'row'}
+				>
+					{categoriesList.map(category => (
+						<Button
+							key={category}
+							className="categories-item"
+							variant="contained"
+						>
+							{category}
+						</Button>
+					))}
+				</Stack>
+			</div>
+		</Fragment>
+	);
+}
 
+/**
+ * @feat - map() | toString() | props
+ */
+function Plants() {
+	const PlantImage = () => {
+		return (
+			<img
+				src={plantImg}
+				alt="plant-img"
+				width="80"
+				className="plant-image img-fluid"
+			/>
+		);
+	};
+
+	const PlantName = props => {
+		const { name, favori } = props;
+		return (
+			<div className="plant-name">
+				{name} {favori ? <p>⭐</p> : <p style={{ visibility: 'hidden' }}>.</p>}
+			</div>
+		);
+	};
+
+	const PlantCategorie = props => {
+		const { category } = props;
+		return (
+			<Stack direction="row" spacing={1}>
+				<Chip label={category} size="small" variant="outlined" />
+			</Stack>
+		);
+	};
+
+	const PlantDescription = props => {
+		const { description } = props;
+		return <p className="plant-description">{description}</p>;
+	};
+
+	const PlantBadge = props => {
+		const { solde } = props;
 		return (
 			<Fragment>
-				<div className="categories-list">
-					<Stack
-						spacing={3}
-						direction={isSmallScreenForCategories ? 'column' : 'row'}
-					>
-						{categoriesList.map(category => (
-							<Button
-								key={category}
-								className="categories-item"
-								variant="contained"
-							>
-								{category}
-							</Button>
-						))}
-					</Stack>
-				</div>
+				{solde ? <div className="solde-badge">solde</div> : null}
 			</Fragment>
 		);
 	};
 
-	/**
-	 * @feat - map()
-	 */
-	const Plants = () => {
-		const PlantImage = () => {
-			return (
-				<img
-					src={plantImg}
-					alt="plant-img"
-					width="80"
-					className="plant-image img-fluid"
-				/>
-			);
-		};
-
-		const PlantName = props => {
-			const { name, favori } = props;
-			return (
-				<div className="plant-name">
-					{name}{' '}
-					{favori ? <p>⭐</p> : <p style={{ visibility: 'hidden' }}>.</p>}
-				</div>
-			);
-		};
-
-		const PlantCategorie = props => {
-			const { category } = props;
-			return (
-				<Stack direction="row" spacing={1}>
-					<Chip label={category} size="small" variant="outlined" />
-				</Stack>
-			);
-		};
-
-		const PlantDescription = props => {
-			const { description } = props;
-			return <p className="plant-description">{description}</p>;
-		};
-
-		const PlantBadge = props => {
-			const { solde } = props;
-			return (
-				<Fragment>
-					{solde ? <div className="solde-badge">solde</div> : null}
-				</Fragment>
-			);
-		};
-
-		/**
-		 * @feat - map() | toString()
-		 */
-		const PlantCondition = props => {
-			const { conditionType, conditionValue } = props;
-			const range = [1, 2, 3];
-
-			return (
-				<div>
-					{range.map(rangeElem =>
-						conditionValue >= rangeElem ? (
-							<span key={rangeElem.toString()}>
-								{conditionType === 'water' ? '💧' : '☀️'}
-							</span>
-						) : null,
-					)}
-				</div>
-			);
-		};
+	const PlantCondition = props => {
+		const { conditionType, conditionValue } = props;
+		const range = [1, 2, 3];
 
 		return (
-			<ul className="plant-list">
-				{plantList.map(plant => (
-					<li key={plant.id} className="plant-item">
-						<PlantImage />
-						<PlantName name={plant.name} favori={plant.isBestSale} />
-						<div className="plant-condition">
-							<PlantCondition
-								conditionType="water"
-								conditionValue={plant.water}
-							/>
-							<PlantCondition
-								conditionType="light"
-								conditionValue={plant.light}
-							/>
-						</div>
-						<PlantCategorie category={plant.category} />
-						<PlantDescription description={plant.description} />
-						<PlantBadge solde={plant.isSpecialOffer} />
-					</li>
-				))}
-			</ul>
+			<div>
+				{range.map(rangeElem =>
+					conditionValue >= rangeElem ? (
+						<span key={rangeElem.toString()}>
+							{conditionType === 'water' ? '💧' : '☀️'}
+						</span>
+					) : null,
+				)}
+			</div>
 		);
 	};
 
 	return (
-		<div id="shopping">
-			<h2 className="header">🌱 Liste des plantes</h2>
-			<div id="categories">
-				<Categories />
-			</div>
-			<div className="body">
-				<Plants />
-			</div>
-		</div>
+		<ul className="plant-list">
+			{plantList.map(plant => (
+				<li key={plant.id} className="plant-item">
+					<PlantImage />
+					<PlantName name={plant.name} favori={plant.isBestSale} />
+					<div className="plant-condition">
+						<PlantCondition
+							conditionType="water"
+							conditionValue={plant.water}
+						/>
+						<PlantCondition
+							conditionType="light"
+							conditionValue={plant.light}
+						/>
+					</div>
+					<PlantCategorie category={plant.category} />
+					<PlantDescription description={plant.description} />
+					<PlantBadge solde={plant.isSpecialOffer} />
+				</li>
+			))}
+		</ul>
 	);
+}
+
+export default class Shopping extends Component {
+	render() {
+		return (
+			<div id="shopping">
+				<h2 className="header">🌱 Liste des plantes</h2>
+				<div id="categories">
+					<Categories />
+				</div>
+				<div className="body">
+					<Plants />
+				</div>
+			</div>
+		);
+	}
 }
