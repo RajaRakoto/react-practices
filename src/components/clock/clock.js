dee0440e88543d269cf284b65c68b47c6d03982b
raja <raja.rakoto7@gmail.com>
2022-05-06 16:56:18 +0300

    add clock component

---
+++ src/components/clock/clock.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+/**
+ * @feat - state | setState() | date | setInterval() | clearInterval() | toLocaleTimeString() | component life cycle
+ */
+export default class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { date: new Date() };
+	}
+
+	// state update
+	tick() {
+		this.setState({
+			date: new Date(),
+		});
+	}
+
+	// timer
+	componentDidMount() {
+		this.timerID = setInterval(() => this.tick(), 1000);
+	}
+
+	// liberation de ressource
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	render() {
+		return <div>{this.state.date.toLocaleTimeString()}</div>;
+	}
+}
