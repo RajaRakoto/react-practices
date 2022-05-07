655a4ffff99e292b2860326cb03dadcfe3b8a3b9
raja <raja.rakoto7@gmail.com>
2022-05-07 15:44:18 +0300

    add pers data

---
+++ src/data/pers.js
@@ -0,0 +1,26 @@
+export const Pers = [
+	{
+		id: 'u1545',
+		pseudo: 'Rasoa',
+		age: 20,
+		email: 'rasoa@protonmail.com',
+		stacks: ['Ansible', 'AWS', 'Docker'],
+		admin: true,
+	},
+	{
+		id: 'u2006',
+		pseudo: 'Rabe',
+		age: 30,
+		email: 'rabe@yahoo.fr',
+		stacks: ['PHP', 'Symfony', 'Laravel', 'MySQL'],
+		admin: false,
+	},
+	{
+		id: 'u4587',
+		pseudo: 'Randria',
+		age: 27,
+		email: 'randria@gmail.com',
+		stacks: ['Flutter', 'Dart'],
+		admin: false,
+	},
+];
