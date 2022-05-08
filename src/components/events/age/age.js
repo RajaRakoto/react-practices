fe8de7fee4cf6150cd644149a014b6b554fed77c
raja <raja.rakoto7@gmail.com>
2022-05-08 10:09:56 +0300

    todo

---
+++ src/components/events/age/age.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Users } from '../../../data/users';
+
+// TODO
+export default function Age() {
+	const state = { Users };
+
+	return <div>{console.log(Users.user1)}</div>;
+}
