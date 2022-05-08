8a8ee7af1bf926ca44c6d2d624e32e5caa55e4c9
raja <raja.rakoto7@gmail.com>
2022-05-08 11:02:23 +0300

    init controlledForm

--- src/components/events/form/sampleForm.js
+++ src/components/events/form/sampleForm.js
@@ -3,10 +3,24 @@ import './form.min.css';
 
 // TODO: verified
 /**
+ * @description - button de validation pour chaque formulaire (independant)
+ */
+const ValidBtn = () => {
+	return (
+		<button className="button-82-pushable">
+			<span className="button-82-shadow"></span>
+			<span className="button-82-edge"></span>
+			<span className="button-82-front text">Valider</span>
+		</button>
+	);
+};
+
+// TODO: verified
+/**
  * @feat - onSubmit() | preventDefault() | get input value
  * @description - formulaire non controlE en React
  */
-export default function SampleForm() {
+function SampleForm() {
 	const handleSubmit = e => {
 		e.preventDefault();
 		alert(e.target['sample-input'].value);
@@ -21,12 +35,46 @@ export default function SampleForm() {
 					name="sample-input"
 					placeholder="Entrer un texte ici ..."
 				/>
-				<button className="button-82-pushable">
-					<span className="button-82-shadow"></span>
-					<span className="button-82-edge"></span>
-					<span className="button-82-front text">Valider</span>
-				</button>
+				<ValidBtn />
 			</form>
 		</React.Fragment>
 	);
+}
+
+/**
+ * @feat - onSubmit() | preventDefault() | get input value
+ * @description - formulaire controllE en React
+ */
+function ControlledForm() {
+	return (
+		<React.Fragment>
+			<form id="controlled-form">
+				<h5>Controlled form</h5>
+				<input
+					type="text"
+					name="controlled-input-name"
+					placeholder="Entrer votre nom ici ..."
+				/>
+				<input
+					type="number"
+					name="controlled-input-age"
+					placeholder="Entrer votre age ici ..."
+					min="12"
+				/>
+				<ValidBtn />
+			</form>
+		</React.Fragment>
+	);
+}
+
+// TODO: verified
+export default class Forms extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<SampleForm />
+				<ControlledForm />
+			</React.Fragment>
+		);
+	}
 }
