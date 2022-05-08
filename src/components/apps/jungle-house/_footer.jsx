591f2b534baac5d9e9b40c8fd485fd30673d9535
raja <raja.rakoto7@gmail.com>
2022-05-08 20:30:39 +0300

    todo: footer component

--- src/components/apps/jungle-house/_footer.jsx
+++ src/components/apps/jungle-house/_footer.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 
+/* datas */
+import githubIco from '../../../assets/icons/github-ico.svg';
+
+/* mui */
+import Button from '@mui/material/Button';
+
 export default class Footer extends React.Component {
 	render() {
 		return (
-			<footer>
-				<h6>Pour les passionné·e·s de plantes 🌿🌱🌵</h6>
-				<div className="email">
-					<label htmlFor="email">Laissez-nous votre e-mail:</label>
-					<input type="text" name="email" />
+			<footer className="container-fluid">
+				<div className="row">
+					<div className="col-12-sm col-md-6">
+						<div className="footer--1">
+							<img src={githubIco} alt="github-link" width={50} />
+							<h6>Pour les passionné(es) de plantes 🌿🌱🌵</h6>
+						</div>
+					</div>
+					<div className="col-12-sm col-md-6">
+						<div className="footer--2">
+							<div id="email" className="input-group input-group-sm">
+								<div className="input-group-prepend">
+									<span className="input-group-text" id="inputGroup-sizing-sm">
+										Laissez-nous votre mail
+									</span>
+								</div>
+								<input
+									name="email"
+									type="text"
+									className="form-control"
+									aria-label="Small"
+									aria-describedby="inputGroup-sizing-sm"
+								/>
+							</div>
+							<Button variant="contained" color="primary" className="email-btn">
+								Enregistrer
+							</Button>
+						</div>
+					</div>
 				</div>
 			</footer>
 		);
