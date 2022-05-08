c538bbf4a3765bf2628cf38d6ee1a04e2803abfb
raja <raja.rakoto7@gmail.com>
2022-05-08 10:18:43 +0300

    SampleForm verified

--- src/components/events/form/sampleForm.js
+++ src/components/events/form/sampleForm.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './form.min.css';
 
+// TODO: verified
 /**
- * @feat - onSubmit() | preventDefault()
+ * @feat - onSubmit() | preventDefault() | get input value
  * @description - formulaire non controlE en React
  */
 export default function SampleForm() {
 	const handleSubmit = e => {
 		e.preventDefault();
-		alert('ValidE !');
+		alert(e.target['sample-input'].value);
 	};
 
 	return (
 		<React.Fragment>
-			<form id="sample-form">
+			<form id="sample-form" onSubmit={handleSubmit}>
 				<h5>Sample form</h5>
 				<input
 					type="text"
 					name="sample-input"
 					placeholder="Entrer un texte ici ..."
-					required
 				/>
-				<button className="button-82-pushable" onSubmit={() => handleSubmit()}>
+				<button className="button-82-pushable">
 					<span className="button-82-shadow"></span>
 					<span className="button-82-edge"></span>
 					<span className="button-82-front text">Valider</span>
