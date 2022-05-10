07e1e5b31c54a0749b16e077e3731e677598582f
raja <raja.rakoto7@gmail.com>
2022-05-10 10:46:11 +0300

    remove inutil file

--- src/components/events/age/age.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Users } from '../../../data/users';
-
-// TODO
-export default function Age() {
-	const state = { Users };
-
-	return <div>{console.log(Users.user1)}</div>;
-}
