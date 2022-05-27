eff4c8c74130da64f308094f8af66703b41e3f46
raja <raja.rakoto7@gmail.com>
2022-05-27 15:14:06 +0300

    valid basic todo app

--- src/components/apps/todo/todo-sample/_task-header.jsx
+++ src/components/apps/todo/todo-sample/_task-header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function TaskHeader() {
+export default function TaskHeader({ tasks }) {
+	const tasksUnChecked = tasks.filter(task => task.isDone === false);
 	return (
 		<React.Fragment>
-			<h2 align="center">TODO list sample - react</h2>
+			<h2 align="center">TODO LIST</h2>
+			<h4 style={{textAlign: "center", fontSize: 22, opacity: 0.4}}>without redux</h4>
+			<h5>Tache(s) a faire: {tasksUnChecked.length}</h5>
 		</React.Fragment>
 	);
 }
